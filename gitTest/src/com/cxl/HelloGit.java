b356cd0768830bf0c9a8de163076d1167513db54
Cxl <1046041105@qq.com>
2020-08-29 16:32:51 +0800

    修改HelloGIt

--- gitTest/src/com/cxl/HelloGit.java
+++ gitTest/src/com/cxl/HelloGit.java
@@ -4,7 +4,9 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git");
-
-
     }
+
+public void testGit(){
+    System.out.println("芜湖~起飞！");
+}
 }
